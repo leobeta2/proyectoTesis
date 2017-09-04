2190a47d9269553407695a56a31f9cb40006101c
leon <leobeta2@gmail.com>
2017-09-04 14:26:22 -0300

    Se agrego una lista y un boton con la opcion de ir a otra pagina... ademas se hizo la instalacion de las librerias de C3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {GraphicPage} from "../graphic/graphic";
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notes = [
+    {id:1, title: "Nota 1", description:"Descripcion de nota 1"},
+    {id:2, title: "Nota 2", description:"Descripcion de nota 2"},
+    {id:3, title: "Nota 3", description:"Descripcion de nota 3"}
+  ];
 
+  @ViewChild('myNav') nav: NavController
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public goToGraphic(){
+    this.navCtrl.push(GraphicPage);
   }
 
 }
