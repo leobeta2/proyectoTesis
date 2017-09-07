38f309ed2bb1150294491dc3c4d4db381995cfa2
leon <leobeta2@gmail.com>
2017-09-07 17:09:56 -0300

    Se ha incorporado la libreria chartjs, ya que c3 presento complicaciones con ionic. Se pueden visualizar tres graficos distintos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,8 @@ import {GraphicPage} from "../graphic/graphic";
 })
 export class HomePage {
   notes = [
-    {id:1, title: "Nota 1", description:"Descripcion de nota 1"},
-    {id:2, title: "Nota 2", description:"Descripcion de nota 2"},
+    {id:1, title: "Graficos Niño", description:"Descripcion de nota 1"},
+    {id:2, title: "Graficos Niña", description:"Descripcion de nota 2"},
     {id:3, title: "Nota 3", description:"Descripcion de nota 3"}
   ];
 
